adf85918fcb642ca0bd68bae5da26c74ce63d8ed
Nick Damiano <damiano.nick@gmail.com>
2017-08-14 21:53:32 +0900

    adds javascript to print out active movies in dom

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,10 +1,10 @@
 class ApiController < ApplicationController
 	# Threshold for number of movie reviews per movie
-	CUTOFF = 35
+	CUTOFF = 30
 
 	# To quickly load landing page, only grabs 5 movies, then ajax calls the rest in the background
 	def get_first_movies
-		@movies = Movie.where("release_date > ?", 2.years.ago).order('random()').joins(:critic_movies).group('movies.id').having("count(movie_id) > #{CUTOFF}")
+		@movies = Movie.where("release_date > ?", 4.years.ago).order('random()').joins(:critic_movies).group('movies.id').having("count(movie_id) > #{CUTOFF}")
 		.limit(5).to_a
 		@movies = @movies.to_a.map(&:serializable_hash).to_json
 		render :json => @movies
@@ -12,7 +12,7 @@ class ApiController < ApplicationController
 
 	# probably want to get above 40 or 45 reviews for the movies asked about, then if those are exhausted, get more with lower number of revies
 	def get_movies
-		@movies = Movie.where("release_date > ?", 3.years.ago).order('random()').joins(:critic_movies).group('movies.id').having("count(movie_id) > #{CUTOFF}")
+		@movies = Movie.where("release_date > ?", 4.years.ago).order('random()').joins(:critic_movies).group('movies.id').having("count(movie_id) > #{CUTOFF}")
 		.limit(250).to_a
 		@movies = @movies.to_a.map(&:serializable_hash).to_json
 		render :json => @movies

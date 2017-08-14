5d355c74d650cd09cb65efe384043108a46172e7
Nick Damiano <damiano.nick@gmail.com>
2017-08-14 19:26:13 +0900

    randomizes movie selection, api calls working for first movies and get movies.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,14 +1,19 @@
 class ApiController < ApplicationController
+	# Threshold for number of movie reviews per movie
+	CUTOFF = 35
 
+	# To quickly load landing page, only grabs 5 movies, then ajax calls the rest in the background
 	def get_first_movies
-		# gets first group because json scraping takes like 4 seconds. so it gets 10 or so and calls the rest at the same time to put into different variable
-		@movies = Movie.where("release_date > ?", 2.years.ago).order(:image_uri).joins(:critic_movies).group('movies.id').having('count(movie_id) > 50').limit(250).to_a
+		@movies = Movie.where("release_date > ?", 2.years.ago).order('random()').joins(:critic_movies).group('movies.id').having("count(movie_id) > #{CUTOFF}")
+		.limit(5).to_a
+		@movies = @movies.to_a.map(&:serializable_hash).to_json
+		render :json => @movies
 	end
+
 	# probably want to get above 40 or 45 reviews for the movies asked about, then if those are exhausted, get more with lower number of revies
 	def get_movies
-		@movies = Movie.where("release_date > ?", 3.years.ago).joins(:critic_movies).group('movies.id').having('count(movie_id) > 10').offset(10)limit(250).to_a
-		binding.pry
-		# TODO it takes too long to load 250 movies so you need to load the first 10 or so, then call to get the rest. 
+		@movies = Movie.where("release_date > ?", 3.years.ago).order('random()').joins(:critic_movies).group('movies.id').having("count(movie_id) > #{CUTOFF}")
+		.limit(250).to_a
 		@movies = @movies.to_a.map(&:serializable_hash).to_json
 		render :json => @movies
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get '/api/movies', to: 'api#get_movies'
+  get '/api/movies/first-movies', to: 'api#get_first_movies'
   get '/api/reviews/initial', to: 'api#get_initial_reviews'
   get '/api/reviews/:id', to: 'api#get_reviews'
 end

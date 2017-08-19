e8905ca76cc36f971fa43e3ecd557b8bd4b77e9f
Nick Damiano <damiano.nick@gmail.com>
2017-08-19 17:23:46 +0900

    a few comments and minor things. commit before checking to new branch

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,6 @@
 class ApiController < ApplicationController
 	# Threshold for number of movie reviews per movie
-	CUTOFF = 40
+	CUTOFF = 35
 
 	# To quickly load landing page, only grabs 5 movies, then ajax calls the rest in the background
 	def get_first_movies
@@ -13,7 +13,7 @@ class ApiController < ApplicationController
 	# probably want to get above 40 or 45 reviews for the movies asked about, then if those are exhausted, get more with lower number of revies
 	def get_movies
 		@movies = Movie.where("release_date > ?", 4.years.ago).order('random()').joins(:critic_movies).group('movies.id').having("count(movie_id) > #{CUTOFF}")
-		.limit(250).to_a
+		.limit(500).to_a
 		@movies = @movies.to_a.map(&:serializable_hash).to_json
 		render :json => @movies
 	end

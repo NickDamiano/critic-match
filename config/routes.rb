1e73a225bed9a16ad4e2b45c55808dff4f28f83a
Nick Damiano <damiano.nick@gmail.com>
2017-08-27 21:34:42 +0900

    changes minimum review rate to 25% from 20%.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get '/api/movies/first-movies', to: 'api#get_first_movies'
   get '/api/reviews/initial', to: 'api#get_initial_reviews'
   get '/api/reviews/:id', to: 'api#get_reviews'
+  get '/critic/:id', to: 'home#critic'
 end

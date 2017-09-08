abad18b244156d270cf9ce3039f58d3266075f32
Nick Damiano <damiano.nick@gmail.com>
2017-09-08 21:22:07 +0900

    adds some basic js to add name to critic page

--- app/assets/javascripts/critic.js
+++ app/assets/javascripts/critic.js
@@ -1,4 +1,20 @@
+// sets or pulls critic_reviews object
+var critics_reviews
+function getCriticsReviews(){
+	if(sessionStorage["criticsReviews"] == undefined ){
+		critics_reviews = {};
+		sessionStorage.setItem('criticsReviews', JSON.stringify(critics_reviews));			
+	}
+	critics_reviews = JSON.parse(sessionStorage['criticsReviews']);
+	return critics_reviews
+}
+
 function criticMain(){
 	var page = window.location.href;
-
+	var critic_id = page.split('/')[4];
+	var critic = critics_reviews[critic_id].name
+	document.getElementById('critic-name').innerHTML = critic
 }
+
+$( document).ready( getCriticsReviews );
+$( document).ready( criticMain );
